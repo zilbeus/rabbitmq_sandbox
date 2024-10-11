16dfed26328731c85e7b0fc4a2605d7637c4f1fa
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-10-11 23:04:02 +0300

    add spring boot rabbitmq message producer project

---
+++ java_spring_producer/src/main/java/monitoring/sandbox/springproducer/SpringproducerApplication.java
@@ -0,0 +1,13 @@
+package monitoring.sandbox.springproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringproducerApplication.class, args);
+	}
+
+}
---
+++ java_spring_producer/src/test/java/monitoring/sandbox/springproducer/SpringproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package monitoring.sandbox.springproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
