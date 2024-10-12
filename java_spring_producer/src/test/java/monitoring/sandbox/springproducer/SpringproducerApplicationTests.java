bcc05bfce827f60dd41349445c7ec891fb74f819
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-10-12 16:45:55 +0300

    modify test class annotation

--- java_spring_producer/src/test/java/monitoring/sandbox/springproducer/SpringproducerApplicationTests.java
+++ java_spring_producer/src/test/java/monitoring/sandbox/springproducer/SpringproducerApplicationTests.java
@@ -3,7 +3,9 @@ package monitoring.sandbox.springproducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import rabbitmq.sandbox.springproducer.SpringProducerApplication;
+
+@SpringBootTest(classes = SpringProducerApplication.class)
 class SpringproducerApplicationTests {
 
 	@Test
